3890e2d9c4ab532593cf9ce5b4963bcdb32926de
Drifter-ZG5 <megadrifter@gmail.com>
2018-10-15 17:43:09 +0300

    v11.1

--- other examples/_1-button-compressor_switch/_1-button-compressor_switch.ino
+++ other examples/_1-button-compressor_switch/_1-button-compressor_switch.ino
@@ -8,51 +8,6 @@ HardwareCAN canBus(CAN1_BASE);
 CanMsg msg ;
 CanMsg *r_msg;
 
-void CANSetup(void) 
-{
-  CAN_STATUS Stat ;
-  // Initialize CAN module
-  canBus.map(CAN_GPIO_PB8_PB9);
-  Stat = canBus.begin(CAN_SPEED_95, CAN_MODE_NORMAL);    
-
-  canBus.filter(0, 0, 0);
-  canBus.set_irq_mode();              // Use irq mode (recommended)
-  Stat = canBus.status();
-  if (Stat != CAN_OK)  
-     {// Initialization failed
-     Serial2.print("Initialization failed");
-     }
-}
-
-CAN_TX_MBX CANsend(CanMsg *pmsg) // Should be moved to the library?!
-{
-  CAN_TX_MBX mbx;
-  do 
-  {
-    mbx = canBus.send(pmsg) ;
-  }
-  while(mbx == CAN_TX_NO_MBX) ;       // Waiting outbound frames will eventually be sent, unless there is a CAN bus failure.
-  return mbx ;
-}
-
-// Send message
-void SendCANmessage(long id=0x100, byte dlength=8, byte d0=0x00, byte d1=0x00, byte d2=0x00, byte d3=0x00, byte d4=0x00, byte d5=0x00, byte d6=0x00, byte d7=0x00)
-{
-  msg.IDE = CAN_ID_STD;         
-  msg.RTR = CAN_RTR_DATA;       
-  msg.ID = id ;                  
-  msg.DLC = dlength;                   // Number of data bytes to follow
-  msg.Data[0] = d0 ;
-  msg.Data[1] = d1 ;
-  msg.Data[2] = d2 ;
-  msg.Data[3] = d3 ;
-  msg.Data[4] = d4 ;
-  msg.Data[5] = d5 ;
-  msg.Data[6] = d6 ;
-  msg.Data[7] = d7 ;
-  CANsend(&msg) ;      // Send this frame
-}
-
 #define PC13ON 0
 #define PC13OFF 1
 #define DELAY 180
@@ -63,7 +18,7 @@ void setup()
 {
 	Serial2.begin(115200); // output to A2 pin
 	Serial2.println("Hello World!");
-	Serial2.println("Starting \"1-button-compressor switch\" v11 final lite");
+	Serial2.println("Starting \"1-button-compressor switch\" v11.1 2018-10-15");
 
 	pinMode(PC13, OUTPUT); // LED
 	digitalWrite(PC13, PC13ON);
@@ -73,39 +28,8 @@ void setup()
 	CANSetup() ;        // Initialize the CAN module and prepare the message structures.
 	Serial2.println("Initialization OK");
 	flag_blocked=false;
-	SendCANmessage(0x100, 0); // wake up bus?
-	SendCANmessage(0x697, 8, 0x47, 0x00, 0x60, 0x00, 0x02, 0x00, 0x00, 0x80); // wake up screen
-}
-
-void AC_trigger()
-{
-	digitalWrite(PC13, PC13ON);
-	delay(DELAY);
-	// turn right 1 click
-	Serial2.println("turn right 1 click");
-	SendCANmessage(0x208, 6, 0x08, 0x16, 0x01); 
-	delay(DELAY);
-	// press
-	Serial2.println("press");
-	SendCANmessage(0x208, 6, 0x01, 0x17, 0x00);
-	delay(30);
-	SendCANmessage(0x208, 6, 0x00, 0x17, 0x00);
-	delay(DELAY);
-
-	// turn left 1 click
-	Serial2.println("turn left 1 click");
-	SendCANmessage(0x208, 6, 0x08, 0x16, 0xff); 
-	delay(DELAY);
-	// turn left 1 click
-	Serial2.println("turn left 1 click");
-	SendCANmessage(0x208, 6, 0x08, 0x16, 0xff); 
-	delay(DELAY);
-	// press
-	Serial2.println("press");
-	SendCANmessage(0x208, 6, 0x01, 0x17, 0x00);
-	delay(30);
-	SendCANmessage(0x208, 6, 0x00, 0x17, 0x00);
-	delay(DELAY);
+  wakeUpBus();
+  wakeUpScreen();
 }
 
 void loop() 
