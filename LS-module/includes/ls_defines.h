d5032a61b54be3d0fd6d8825aea60709aa056d02
Megadrifter-fujitsu <megadrifter@gmail.com>
2019-09-26 14:15:38 +0300

    - fix key position read - another small fix

--- LS-module/includes/ls_defines.h
+++ LS-module/includes/ls_defines.h
@@ -1,8 +1,20 @@
 #define LS_ID_KEY 0x170
 
 #define LS_KEY_DATA_BYTE 0x00
-#define KEY_LOCKED 0x30
-#define KEY_IGN_OFF 0x70
-#define KEY_IGN_ON 0x72
-#define KEY_STARTER_ON 0x74
+
+// no key
+#define KEY_OUT 0x30
+
+// key in , +0x160#02.00.xx.xx
+#define KEY_LOCKED 0x70
+#define KEY_IN 0x70
+// unlocked, wake up
+#define KEY_IGN_OFF 0x72
+#define KEY_UNLOCKED 0x72
+
+// ignition just swithed on
+#define KEY_IGN_ON 0x74
+// ignition is on
 #define KEY_STARTER_OFF 0x54
+
+#define KEY_STARTER_ON 0x76
